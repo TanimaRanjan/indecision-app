cdb494e37ac86b96fa3d5a5ed2f21186e1f21cc1
Tanima Ranjan <tanimaranjan@gmail.com>
2018-11-28 20:58:51 -0500

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,13 @@
 import React from 'react'
 
-
-const Header = (props) => 
-     (
-         
+const Header = (props) => (
         <div className="header">
-        <div className="container">
-            <h1 className="header__title">{props.title}</h1>       
-            {props.subTitle &&  <h2 className="header__subtile">{props.subTitle}</h2> }
-        </div>
+            <div className="container">
+                <h1 className="header__title">{props.title}</h1>       
+                {props.subTitle &&  <h2 className="header__subtile">{props.subTitle}</h2> }
+            </div>
         </div>
     )
-
 
 Header.defaultProps = {
 title:'Indecision'
