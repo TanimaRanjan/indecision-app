a05020783a5503a29c54bc2050044af2df7dfdcf
Tanima Ranjan <tanimaranjan@gmail.com>
2018-11-28 20:59:15 -0500

    Main page

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import AddOption from './AddOption'
-// import Option from './components/Option'
 import Options from './Options'
 import Header from './Header'
 import Action from './Action'
